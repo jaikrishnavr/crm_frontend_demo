ab3e31139687c4cf8aeebba9b30b9064fcfe2904
jaikrishnavr <jaikrishnagokul@gmail.com>
2023-05-17 21:52:13 +0530

    third stage of crm login sign page with api(.env) and react router dom updatewith jwt

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import { userSignIn, userSignUp } from "../api/auth";
-
 
 // This function defines the Login component.
 function Login() {
@@ -23,6 +22,23 @@ function Login() {
   // This state variable stores whether an error has occurred.
   const [error, setError] = useState(false);
   // This function toggles between the sign up and log in forms.
+
+  useEffect(() => {
+    const userType = localStorage.getItem("userType");
+    const token = localStorage.getItem("token");
+
+    if (!token || !userType) {
+      return;
+    }
+
+    if (userType === "ENGINEER") {
+      window.location.href = "/engineer";
+    } else if (userType === "CUSTOMER") {
+      window.location.href = "/customer";
+    } else {
+      window.location.href = "/admin";
+    }
+  }, []);
 
   const toggleSignup = () => {
     setShowSignup(!ShowSignup);
@@ -64,14 +80,12 @@ function Login() {
 
     // Make an API call to sign up the user.
 
-    
-
     userSignUp(data)
       .then((res) => {
         console.log(res);
         setError(false);
         setMessage("SignUp successful");
-        window.location.href = "/"
+        window.location.href = "/";
       })
       .catch((err) => {
         if (err.response.status === 400) {
@@ -89,7 +103,7 @@ function Login() {
     const data = { userId, password };
     e.preventDefault();
 
-     // Make an API call to log in the user. 
+    // Make an API call to log in the user.
 
     userSignIn(data)
       .then((res) => {
@@ -97,18 +111,22 @@ function Login() {
         setError(false);
         setMessage("Login Successful");
 
-        if(res.data.userType === "ENGINEER") {
-          window.location.href="/engineer";
-        }
+        localStorage.setItem("name", res.data.name);
+        localStorage.setItem("userId", res.data.userId);
+        localStorage.setItem("email", res.data.email);
+        localStorage.setItem("userStatus", res.data.userStatus);
+        localStorage.setItem("token", res.data.accessToken);
+        localStorage.setItem("userType", res.data.userType);
 
-        else if(res.data.userType === "CUSTOMER"){
-          window.location.href = "/customer"
+        if (res.data.userType === "ENGINEER") {
+          window.location.href = "/engineer";
+        } else if (res.data.userType === "CUSTOMER") {
+          window.location.href = "/customer";
+        } else {
+          window.location.href = "/admin";
         }
-        else{
-          window.location.href="/admin"
-        }
-
       })
+      
       .catch((err) => {
         if (err.response.status) {
           setError(true);
@@ -136,8 +154,8 @@ function Login() {
   };
 
   const handleSelect = (e) => {
-setUserType(e);
-  }
+    setUserType(e);
+  };
 
   // This function returns the Login component.
   return (
