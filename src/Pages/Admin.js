7d0c2a00620b40320f75fb5391462fe70c1c4706
jaikrishnavr <jaikrishnagokul@gmail.com>
2023-05-18 11:14:06 +0530

    stage four side nav bar (using coreui react) update in admin.js ui

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import Sidebar from '../Components/Sidebar'
+import React from "react";
+import Sidebar from "../Components/Sidebar";
 
 function Admin() {
-  return (
-    <div>
-      <Sidebar/>
 
-      <h2>Admin Dashboard</h2>
+  const userName = localStorage.getItem("name");
+  return (
+    <div className="row bg-light">
+      <div className="col-1">
+        <Sidebar />
+      </div>
+
+      <div className="col vh-100 m-4">
+        <div className="container">
+        <h3 className="text-primary text-center">Welcome, {userName} </h3>
+        <p className="text-center text-muted">Take a quick look at your admin status below </p>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
